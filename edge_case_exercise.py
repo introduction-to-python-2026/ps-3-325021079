221e8aaf2539e99f568d85330df1b2c1a15eb0bb
325021079 <shenhavshkalim@gmail.com>
2025-11-12 21:34:33 +0200

    Update edge_case_exercise.py

--- edge_case_exercise.py
+++ edge_case_exercise.py
@@ -2,18 +2,15 @@ def move(my_list, direction=None):
    
     index_of_one = my_list.index(1)
   
-    #אם אין ערך לכיוון
+    
     if direction is None:
         return my_list
-   #אם יש רק ערך 1 שהוא 1 
     if my_list[0] == 1 and len(my_list) == 1 :
         return my_list
 
-    #אם קצה שמאל הוא 1
     if index_of_one == 0 and direction == "left" :
         return my_list
   
-    #אם קצה ימין הוא 1
     if index_of_one == len(my_list) -1 and direction == "right" :
         return my_list
         
@@ -25,4 +22,4 @@ def move(my_list, direction=None):
           my_list[index_of_one] = 0
           my_list[index_of_one - 1] = 1
 
-    return my_list
+return my_list
